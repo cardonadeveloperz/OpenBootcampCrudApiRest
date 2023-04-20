5759d7df289ab3ece6b1aa6e944f067d1c3afa7f
CardonaDeveloperZabud <julio.cardona@zabud.com.co>
2023-04-20 15:34:36 -0500

    Commit inicial, se creo el proyecto con en start.spring.io

---
+++ src/test/java/com/example/OpenBootcampCrudApiRest/OpenBootcampCrudApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OpenBootcampCrudApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBootcampCrudApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
